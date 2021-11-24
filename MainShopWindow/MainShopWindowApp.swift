a92a9f476a0d2e323c422e38f5be2dbb67e2b398
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-24 19:40:11 +0200

    Initial Commit

---
+++ MainShopWindow/MainShopWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainShopWindowApp.swift
+//  MainShopWindow
+//
+//  Created by Vahtee Boo on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MainShopWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
